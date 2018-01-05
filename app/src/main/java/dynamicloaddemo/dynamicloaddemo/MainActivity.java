98bbe4703042961c033003b3504ea015700d9648
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2018-01-05 17:52:02 +0800

    Initial commit

---
+++ app/src/main/java/dynamicloaddemo/dynamicloaddemo/MainActivity.java
@@ -0,0 +1,18 @@
+package dynamicloaddemo.dynamicloaddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/****
+ * http://blog.csdn.net/singwhatiwanna/article/details/22597587
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
